467844d81a04bc9c6309312199d4f27cd358b027
px-x-h <robik.ua123@gmail.com>
2025-02-06 20:03:57 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,3 +7,19 @@ window.addEventListener('scroll', function(){
     fixedHeader.classList.remove('fixed');
   }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sentences = document.querySelectorAll('.sentence');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, {threshold: 0.5 });
+
+  sentences.forEach(sentence => observer.observe(sentence));
+});
